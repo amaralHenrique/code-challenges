38c6c30be539611780b4b82bfb9a7eaf144b4770
Henrique Amaral <amaralhqz@gmail.com>
2022-01-23 21:09:02 -0300

    Project Creation

---
+++ jumnia-phone-manager/src/main/java/com/personalprojects/phonemanager/JumniaPhoneManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personalprojects.phonemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumniaPhoneManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumniaPhoneManagerApplication.class, args);
+	}
+
+}
---
+++ jumnia-phone-manager/src/test/java/com/personalprojects/phonemanager/JumniaPhoneManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalprojects.phonemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumniaPhoneManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
