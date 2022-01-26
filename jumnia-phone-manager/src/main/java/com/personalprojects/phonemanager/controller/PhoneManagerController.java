b5b9dd1571efc521fa1e9854bb55a8f281cf372b
Henrique Amaral <amaralhqz@gmail.com>
2022-01-25 23:54:56 -0300

    Removing unnecessary endpoint

--- jumnia-phone-manager/src/main/java/com/personalprojects/phonemanager/controller/PhoneManagerController.java
+++ jumnia-phone-manager/src/main/java/com/personalprojects/phonemanager/controller/PhoneManagerController.java
@@ -1,11 +1,7 @@
 package com.personalprojects.phonemanager.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.personalprojects.phonemanager.dto.FilterDTO;
 import com.personalprojects.phonemanager.dto.PageDTO;
-import com.personalprojects.phonemanager.entities.Customer;
-import com.personalprojects.phonemanager.repositories.CustomerRepository;
 import com.personalprojects.phonemanager.services.PhoneManagerService;
 
 @RestController
@@ -22,27 +16,13 @@ import com.personalprojects.phonemanager.services.PhoneManagerService;
 public class PhoneManagerController {
 
 	@Autowired
-	private CustomerRepository repository;
-	
-	@Autowired
 	private PhoneManagerService pmService;
-	
-	@GetMapping
-	public ResponseEntity<List<Customer>> findAll(){
 		
-		List<Customer> list = repository.findAll();
-		
-		return new ResponseEntity<List<Customer>>(list,HttpStatus.OK);
-		
-	}
-	
 	@PostMapping(path = "/retrieve/list")
 	public ResponseEntity<PageDTO> getListedPage(@RequestBody(required = false) FilterDTO filter) throws Exception {
 		
 		return ResponseEntity.ok(pmService.getPagedList(filter));
 		
 	}
-	
-	
-	
+		
 }
